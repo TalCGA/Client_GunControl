d5d7c7ed2e0c88c0dcc54c871f9a8f5fea77c66f
talch <talch@DESKTOP-AC9KK1T>
2019-01-06 11:44:15 +0200

    All map related and some aadjustments

---
+++ Client/Scripts.js
@@ -0,0 +1,5 @@
+// JavaScript source code
+const userAction = async () => {
+    const response = await fetch('');
+    const myJson = await response.json(); //extract JSON from the http response
+}
