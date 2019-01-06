32e1a58cb98be15cf2b8fdcb5abee6162e55b38e
talch <talch@DESKTOP-AC9KK1T>
2019-01-06 13:24:54 +0200

    Fix the API request to all Tweets

--- Client/Scripts.js
+++ Client/Scripts.js
@@ -1,5 +1,19 @@
 // JavaScript source code
-const userAction = async () => {
-    const response = await fetch('');
-    const myJson = await response.json(); //extract JSON from the http response
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest();
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://guncontrol.herokuapp.com/getAllTweets', true);
+
+request.onload = function () {
+    // Begin accessing JSON data here
+    var TweetsJson = JSON.parse(this.response);
+
+    TweetsJson.forEach(tweet => {
+        // Log each movie's title
+        console.log(tweet.location);
+    });
 }
+
+// Send request
+request.send();
